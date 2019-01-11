b46bbaa09e60bd3ffc81c6096f41c33ddf32e736
annettesunday <annette.odhiambo@andela.com>
2019-01-11 17:36:49 +0300

    ch(app): setup express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req,res) => {
+res.status(200).send({message: 'Welcome to Population Management System'})
+})
+
+app.get('*', (req,res)=> {
+  res.status(400).send({message: 'This action cannot be executed. Got to /api to see all available routes'})
+})
+
+app.listen(port, () => console.log('Yaay. PMS is up!'))
