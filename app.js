eed1cc19ce65d2e83b53176adb95a41ff8ebac2e
annettesunday <annette.odhiambo@andela.com>
2019-01-14 07:28:35 +0300

    ch(location): create dummy controllers

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
+// setup the express app
 const app = express();
+
+
+//Log requests to the console
+app.use(logger('dev'));
+
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 8000;
 
