4b38d34ea1ba08e7c9fefecd1e8434c8dc72f827
annettesunday <annette.odhiambo@andela.com>
2019-01-22 16:37:48 +0300

    ch(locations): refactor code

--- models/locations.js
+++ models/locations.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       unique: {
           args: true,
-          msg: 'Location already exits'
+          msg: 'Location already exists'
       },
       validate: {
         notEmpty: {
